63775adc9c6fcdcec44ff1446bc27c43e35ae795
Rafal <rafaljanko@gmail.com>
2022-11-04 17:58:49 +0100

    adding first view

--- ConsoleGamesPortal/urls.py
+++ ConsoleGamesPortal/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from portal.views import TimerView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('timer/', TimerView, name='timer'),
 ]
--- portal/views.py
+++ portal/views.py
@@ -2,3 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 
+def TimerView(request):
+    return render(request, "portal/timer.html")
